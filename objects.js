b1afc000119cbf096294a0103766cb528011191f
noadrow <noadraw@gmail.com>
2016-05-06 09:19:08 +0300

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,7 @@
+var canvas = document.getElementById("canvas"),
+    ctx = canvas.getContext("2d"),
+    width = canvas.width,
+    height = canvas.height;
+
+var currentInput = new Array(),
+    options = ["aaa", "aab", "bbb"];
