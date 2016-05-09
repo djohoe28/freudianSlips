afe1ca827e05139b2c9e1fca8ec37fdd75450222
noadrow <noadraw@gmail.com>
2016-05-09 18:44:26 +0300

    basic prototype update(1)
    more effective code

--- functions.js
+++ functions.js
@@ -1,18 +1,18 @@
 function getScore(){
-    var answerValue = 0;
+    var mistakes = 0;
     for(i=0; i<answer.length; i++){
-        answerValue += answer[i].index;
-    };
-    console.log(answerValue);
-
-    if(answer[0].index===answer[1].index && answer[1].index===answer[2].index){
-        answerValue = 0;
+        if(answer[i-1]!== undefined){
+            if(answer[i-1].index!==answer[i].index){
+                mistakes++;
+                console.log(mistakes);
+            }
+        }
     };
     
-    switch(answerValue){
-        case 0: case 6: score = "perfect!"; break;
-        case 1: case 2: case 4: score = "good enough"; break;
-        case 3: score = "well... you'r suck"; break;
+    switch(mistakes){
+        case 0: score = "perfect!"; break;
+        case options.length - 1: score = "well... you'r suck"; break;
+        default: score = "good enough"; break;
     }
 
 }
